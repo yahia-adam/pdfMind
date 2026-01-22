0536547281c63fdfc8700a5c8f13e922798fdf06
yahia abdchafee adam <adam.Y.abdc@gmail.Com>
2026-01-22 20:53:31 +0100

    remove unsused file

--- src/data/embedding.py
+++
@@ -1,8 +0,0 @@
-from langchain_ollama import OllamaEmbeddings
-
-# 4. Initialize Embeddings
-def create_embedding(model_name):
-    embeddings = OllamaEmbeddings(model=model_name)
-    if not embeddings:
-        raise ValueError(f"Failed to create embeddings for model: {model_name}")
-    return embeddings
